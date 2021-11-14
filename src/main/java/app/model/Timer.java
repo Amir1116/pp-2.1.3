b1e4203b431139e4a235ed674f615b9e519d9c6b
денис эм <denisem@deniss-MacBook-Pro.local>
2021-11-14 23:38:26 +0300

    pp-2.1.2

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("timer")
-
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
